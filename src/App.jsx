3f971c7c1e0c57248443aacd9f4242ec08ce853d
tawhid126 <tawhidurrahman830@gmail.com>
2025-09-07 12:20:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>Todo List Lite</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li
+            key={index}
+            onClick={() => removeTask(index)}
+            style={{ cursor: "pointer" }}
+          >
+            {t}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
